e0be12f4c1734dd98a280a7346641f08e6294c13
Shohruh003 <shohruhazimov003@gmail.com>
2022-04-10 23:37:52 +0500

    git init

---
+++ js/script.js
@@ -0,0 +1,9 @@
+alert("Salom, xush kelibsiz!");
+
+var firstName = prompt("Ismingiz nima!");
+
+var paragraph = document.createElement("p");
+
+paragraph.textContent = firstName;
+
+document.body.appendChild(paragraph);
