0d5e6e5bc88fa45efe45627029d6683a49ebadc5
Shohruh003 <shohruhazimov003@gmail.com>
2022-04-11 01:01:46 +0500

    js stile

--- js/script.js
+++ js/script.js
@@ -4,6 +4,12 @@ var firstName = prompt("Ismingiz nima!");
 
 var paragraph = document.createElement("p");
 
-paragraph.textContent = firstName;
+paragraph.textContent = "Sizning ismingiz : " + firstName;
 
 document.body.appendChild(paragraph);
+
+document.querySelector("p").style.fontSize = "36px";
+
+document.querySelector("p").style.textAlign = "center";
+
+document.querySelector("p").style.color = "red";
